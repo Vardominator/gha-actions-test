b512fc302dc9ae674808f9ce5a1b68eb70e0c047
Vardominator <barsegyanvarderes@gmail.com>
2020-08-06 23:39:36 -0700

    test add key

--- sign_image.sh
+++ sign_image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo $NOTARY_SIGNER_DELEGATION > /tmp/delegation.pub
-
+cat /tmp/delegation.pub | tr ' ' '\n'
 chmod 600 /tmp/delegation.pub
 
 docker trust signer add --key /tmp/delegation.pub gha_signer 263799606133.dkr.ecr.us-e
