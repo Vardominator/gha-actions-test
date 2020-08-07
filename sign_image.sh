43506d808263610a38a1123d1706d2872b7591ec
Vardominator <barsegyanvarderes@gmail.com>
2020-08-07 00:54:00 -0700

    cleanup again

--- sign_image.sh
+++ sign_image.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+# prepare signer delegation key
 echo $NOTARY_SIGNER_DELEGATION > /tmp/encoded.pub
 cat /tmp/encoded.pub | base64 --decode > /tmp/decoded.pub
 chmod 600 /tmp/decoded.pub
 
+# add signer
 docker trust signer add --key /tmp/decoded.pub gha_signer 263799606133.dkr.ecr.us-east-1.amazonaws.com/notary/signtest2
 
 # build
+# ...
 
 docker trust sign 263799606133.dkr.ecr.us-east-1.amazonaws.com/notary/signtest2:latest
 
 # push
+# ...
 
-ls ~/.docker/trust
-
-# rm /tmp/delegation.pub
+# cleanup
+rm /tmp/delegation.pub
