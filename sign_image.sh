8f4cc30b5b51357f773a81d9ed5c9d202270c7a4
Vardominator <barsegyanvarderes@gmail.com>
2020-08-06 23:15:25 -0700

    test key load

--- sign_image.sh
+++ sign_image.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo $NOTARY_SIGNER_DELEGATION
+echo $NOTARY_SIGNER_DELEGATION > /tmp/delegation.pub
+
+docker trust key load /tmp/delegation.pub
+
+ls ~/.docker/trust
+
+rm /tmp/delegation.pub
