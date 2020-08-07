1f50fa71ba5105df339e5d67cd24a81ba39b94d8
Vardominator <barsegyanvarderes@gmail.com>
2020-08-06 23:46:36 -0700

    decode encode

--- sign_image.sh
+++ sign_image.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo $NOTARY_SIGNER_DELEGATION > /tmp/delegation.pub
-cat /tmp/delegation.pub | tr ' ' '\n'
-chmod 600 /tmp/delegation.pub
+echo $NOTARY_SIGNER_DELEGATION > /tmp/encoded.pub
+cat /tmp/encoded.pub | base64 --decode > /tmp/decoded.pub
+chmod 600 /tmp/decoded.pub
 
-docker trust signer add --key /tmp/delegation.pub gha_signer 263799606133.dkr.ecr.us-e
+docker trust signer add --key /tmp/decoded.pub gha_signer 263799606133.dkr.ecr.us-e
 
 ls ~/.docker/trust
 
