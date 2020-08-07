9d426b4fadc372b678430d94adfe69fc75d51102
Vardominator <barsegyanvarderes@gmail.com>
2020-08-07 01:00:56 -0700

    final test

--- sign_image.sh
+++ sign_image.sh
@@ -6,7 +6,7 @@ cat /tmp/encoded.pub | base64 --decode > /tmp/decoded.pub
 chmod 600 /tmp/decoded.pub
 
 # add signer
-docker trust signer add --key /tmp/decoded.pub gha_signer 263799606133.dkr.ecr.us-east-1.amazonaws.com/notary/signtest2
+docker trust signer add --key /tmp/decoded.pub ${SIGNER_NAME} ${IMAGE_REPOSITORY}
 
 # build
 # ...
