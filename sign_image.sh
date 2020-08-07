00d9a87c727283a604dc2654bf743030485cfd03
Vardominator <barsegyanvarderes@gmail.com>
2020-08-07 00:18:17 -0700

    decode encode

--- sign_image.sh
+++ sign_image.sh
@@ -4,7 +4,7 @@ echo $NOTARY_SIGNER_DELEGATION > /tmp/encoded.pub
 cat /tmp/encoded.pub | base64 --decode > /tmp/decoded.pub
 chmod 600 /tmp/decoded.pub
 
-docker trust signer add --key /tmp/decoded.pub gha_signer 263799606133.dkr.ecr.us-e
+docker trust signer add --key /tmp/decoded.pub gha_signer 263799606133.dkr.ecr.us-east-1.amazonaws.com/notary/signtest2
 
 ls ~/.docker/trust
 
